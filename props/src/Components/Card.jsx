b66ea6a1a0c8ae4f0a6d3628245477d9b89ec22d
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-12-03 14:06:05 +0530

    blog api added

--- props/src/Components/Card.jsx
+++ props/src/Components/Card.jsx
@@ -1,56 +1,51 @@
 import React, { useEffect, useState } from "react";
 
 // const photoUrl = "https://api.slingacademy.com/v1/sample-data/photos" // Api
-// "https://picsum.photos/seed/picsum/200/300",
 
 const Card = () => {
   const [photos, setphotos] = useState([]);
+  const [loading, setloading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://api.slingacademy.com/v1/sample-data/photos"
+          "https://api.slingacademy.com/v1/sample-data/blog-posts?offset=5&limit=30"
         );
         const data = await response.json(); //Error: TypeError: photos.map is not a function, photos is not an array use if-else
 
-        if (Array.isArray(data.photos)) {
+        if (Array.isArray(data.blogs)) {
           //Data received from the API is not an array it is object
-          setphotos(data.photos);
-
+          setphotos(data.blogs);
         } else {
           console.error("Data received from the API is not an array:", data);
         }
       } catch (error) {
         console.log("Datat error", error);
+      } finally {
+        setloading(false);
       }
     };
     fetchData();
   }, []);
   return (
     <div className="main-container">
+      <h1>Daiy Blogs</h1>
       <div className="cardDiv">
-        {
-        photos.map((photo) => (
-          <div className="imgDiv">
-            <img src={photo.url} />
-            <p>{photo.title}</p>
-          </div>
-        ))
-        }
+        {loading ? (
+          <img src="https://i.gifer.com/origin/b1/b128497bc8d6fcfefb93b88b260561a7_w200.gif" />
+        ) : (
+          photos.map((blog) => (
+            <div className="imgDiv">
+              <h3 className="title">{blog.title}</h3>
+              <img src={blog.photo_url} />
+              <p>{blog.description}</p>
+              <p>{blog.content_text}</p>
+            </div>
+          ))
+        )}
       </div>
     </div>
-    // <div className="main-container">
-    //   <h1>Photo Gallery</h1>
-    //   <ul className="container">
-    //     {photos.map((photo) => (
-    //       <li key={photo.id} className="imgDiv">
-    //         <img src={photo.url} alt={photo.title} />
-    //         <p>{photo.title}</p>
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
   );
 };
 
