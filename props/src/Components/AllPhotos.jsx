68c00114d2d621cff3df7aa5da032304facf159c
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-12-04 22:23:27 +0530

    download function added

--- props/src/Components/AllPhotos.jsx
+++ props/src/Components/AllPhotos.jsx
@@ -1,41 +1,50 @@
-import React, { useEffect, useState } from 'react'
-import ImageCard from './ImageCard'
+import React, { useEffect, useState } from "react";
+import ImageCard from "./ImageCard";
 //Api for blogs "https://api.slingacademy.com/v1/sample-data/blog-posts?offset=5&limit=30"
 const AllPhotos = () => {
-  const [photos, setphotos] = useState([])
+  const [photos, setphotos] = useState([]);
+  const [loading, setloading] = useState(true);
 
-  useEffect(()=>{
-    const fetchData = async ()=>{  
+  useEffect(() => {
+    const fetchData = async () => {
       try {
-        let response = await fetch("https://api.slingacademy.com/v1/sample-data/photos")
-        const data = await response.json()
-        if(Array.isArray(data.photos)){
-          setphotos(data.photos)
-          
-        }else{
-          console.log(error, "error from api")
+        let response = await fetch(
+          "https://api.slingacademy.com/v1/sample-data/photos"
+        );
+        const data = await response.json();
+        if (Array.isArray(data.photos)) {
+          setphotos(data.photos);
+        } else {
+          console.log(error, "error from api");
         }
-        
       } catch (error) {
-        console.log(error, "error from api")
+        console.log(error, "error from api");
+      } finally {
+        setloading(false);
       }
-    }
-    fetchData()
-  },[])
-  
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className='main-container'>
-        <h1>Photos Gallary</h1>
-      <div className='cardDiv'>
-        {
-          photos.map((blog)=>(
-            <ImageCard key={blog.id} imageUrl={blog.url} title={blog.title} decription={blog.description}/>
+    <div className="main-container">
+      <h1>Photos Gallary</h1>
+      <div className="cardDiv">
+        {loading ? (
+          <img src="https://media.tenor.com/JBgYqrobdxsAAAAi/loading.gif" />
+        ) : (
+          photos.map((image) => (
+            <ImageCard
+              key={image.id}
+              imageUrl={image.url}
+              title={image.title}
+              decription={image.description}
+            />
           ))
-        }
-        
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllPhotos
+export default AllPhotos;
--- props/src/Components/ImageCard.jsx
+++ props/src/Components/ImageCard.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 
-const ImageCard = ({imageUrl, decription, title}) => {
-  const handleDownload = () =>{
-    window.open(imageUrl, "_blank")
-  }
+const ImageCard = ({ imageUrl, decription, title }) => {
+  const handleView = () => {
+    window.open(imageUrl, "_blank");
+  };
+  const handleDownload = () => {
+    const downloadLink = document.createElement("a"); //The handleDownload function creates a temporary <a> element.
+    downloadLink.href = imageUrl;   //It sets the href attribute of the anchor to the image URL.
+    downloadLink.download = "Image";
+
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+
+    document.body.removeChild(downloadLink);
+  };
+
   return (
     <div className="imgDiv">
       <h3>{title}</h3>
-      <img src={imageUrl} onClick={handleDownload}/>
+      <img src={imageUrl} onClick={handleView} />
       <p>{decription}</p>
-      <button className="download-btn" onClick={handleDownload}>download</button>
+      <button className="download-btn" onClick={handleDownload}>
+        download
+      </button>
     </div>
   );
 };
