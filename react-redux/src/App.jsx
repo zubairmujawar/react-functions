450b2c17c4bf34e07b55474f7c9cb49b2765511c
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2023-12-20 22:57:04 +0530

    Redux project started

---
+++ react-redux/src/App.jsx
@@ -0,0 +1,16 @@
+import "./styles/app.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./Components/Home";
+import { Hearder } from "./Components/Hearder";
+function App() {
+  return (
+    <BrowserRouter>
+      <Hearder />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-redux/src/Components/Hearder.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Hearder = () => {
+  return <div>Hearder</div>;
+};
